0ae54430987aaad4bdebb5dfc12edc9c8d18f7e2
CV5S <91hukuro@gmail.com>
2025-12-20 19:22:19 +0900

    adjust url

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const footer = document.createElement("footer");
   footer.innerHTML = `
     <div class="footer-right">
-      <a href="/index.html">県立印旛諸根具足センター</a>にもどる
+      <a href="/inba/index.html">県立印旛諸根具足センター</a>にもどる
        / 
       <a href="https://obsolete.hatenadiary.com/" target="_blank" rel="noopener">良性腫瘍</a>
       / 
